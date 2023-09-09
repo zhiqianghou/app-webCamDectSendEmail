4f2826f08657d576943467da2d38cc7f033918f7
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-09 09:47:36 -0500

    Add send_email function in emailing.py

--- emailing.py
+++ emailing.py
@@ -1,10 +1,29 @@
 # Don't use email.py as file name as python has email library.
-def send_email():
-	print("Test passed!")
+import smtplib, ssl
+import imghdr
+from email.message import EmailMessage
 
 
+PASSWORD = "pwoojjssywjkpply"
+SENDER = "houzh.py@gmail.com"
+RECEIVER = "houzh.py@gmail.com"
 
+def send_email(image_object):
+	email_message = EmailMessage()
+	email_message["Subject"] = "New customer showed up"
+	email_message.set_content("Hi, We just saw a new customer")
 
+	with open(image_object, "rb") as file:
+		content = file.read()
+
+	email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+	gmail = smtplib.SMTP("smtp.gmail.com", 587)
+	gmail.ehlo()
+	gmail.starttls()
+	gmail.login(SENDER, PASSWORD)
+	gmail.sendail(SENDER, RECEIVER, email_message.as_string())
+	gmail.quit()
 
 
 if __name__ == "__main__":
